3fef53469a147832d490d81f6237c63ba650997f
ldept <mlody26798@gmail.com>
2022-01-08 09:35:01 +0100

    Initial repo model added

---
+++ app/src/main/java/com/ldept/restapidemo/data/models/GithubRepo.kt
@@ -0,0 +1,17 @@
+package com.ldept.restapidemo.data.models
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class GithubRepo (
+        val id: Int,
+        val name: String,
+        val description: String?,
+        val visibility: String,
+        // Github Repository API url
+        val url: String,
+        val stargazers_count: Int,
+        val watchers_count: Int,
+        val language: String,
+) : Parcelable
