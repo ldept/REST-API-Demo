d6e85ba5dd72bba314bf88e2b6f12185a8a6494b
ldept <mlody26798@gmail.com>
2022-01-08 10:18:27 +0100

    Initial UI for repo list fragment

--- app/src/main/java/com/ldept/restapidemo/ui/MainActivity.kt
+++ app/src/main/java/com/ldept/restapidemo/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ldept.restapidemo
+package com.ldept.restapidemo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ldept.restapidemo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/ldept/restapidemo/ui/repoListFragment/RepoListFragment.kt
@@ -0,0 +1,25 @@
+package com.ldept.restapidemo.ui.repoListFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ldept.restapidemo.R
+
+class RepoListFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_repo_list, container, false)
+    }
+
+}
