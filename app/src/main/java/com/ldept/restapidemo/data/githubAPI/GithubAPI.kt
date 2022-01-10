5ff96054912776a7ae7516c6370756ed5f8ab49c
ldept <mlody26798@gmail.com>
2022-01-10 11:41:13 +0100

    Defined accepted API version

--- app/src/main/java/com/ldept/restapidemo/data/githubAPI/GithubAPI.kt
+++ app/src/main/java/com/ldept/restapidemo/data/githubAPI/GithubAPI.kt
@@ -4,6 +4,7 @@ import com.ldept.restapidemo.data.models.GithubFile
 import com.ldept.restapidemo.data.models.GithubRepo
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -12,12 +13,14 @@ interface GithubAPI {
     companion object {
         const val BASE_URL =  "https://api.github.com/"
     }
+    @Headers("Accept: application/vnd.github.v3+json")
     @GET("users/allegro/repos")
     suspend fun getAllegroRepos(
         @Query("page") page: Int,
         @Query("per_page") perPage: Int,
     ) : List<GithubRepo>
 
+    @Headers("Accept: application/vnd.github.v3+json")
     @GET("/repos/allegro/{name}/contents")
     suspend fun getAllegroRepoFiles(
         @Path("name") name: String
